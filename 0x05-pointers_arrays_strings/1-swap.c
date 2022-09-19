e5087e8c21f9bd604303955066bfc50149d490ee
root <alishettimamusty@gmail.com>
2022-09-19 06:04:45 -0700

    Adding function that swaps two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,13 @@
-
+/**
+ * swap_int - swaps two integer values
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: No return value
+ */
+void swap_int(int *a, int *b)
+{
+	int swp = *a;
+	*a = *b;
+	*b = swp;
+}
